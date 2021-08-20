b546da117c87aee7af687885ce0353f7bc4e5110
pcifuentes97 <73726542+pcifuentes97@users.noreply.github.com>
2021-08-20 15:52:43 -0400

    Add files via upload

---
+++ TableViewApp/TableViewApp/Modules/ImageList/Controller/ImageListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ImageListViewController.swift
+//  TableViewApp
+//
+//  Created by Paolo Cifuentes on 8/20/21.
+//
+
+import UIKit
+
+class ImageListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }    
+}
+
